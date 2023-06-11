7648bb15a6f5b78da4ebd6a3751cf9521da727d7
Farfa7886 <50551069+Farfa7886@users.noreply.github.com>
2023-06-11 14:28:58 +0200

    Update Chat.vue

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -9,10 +9,6 @@ import axios from 'axios';
       <form method="dialog" class="modal-box">
         <button for="chatModal" class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
         <h3 class="font-bold text-lg" style="margin-bottom: 50px">Chat</h3>
-        <div class="alert alert-success" id="chat-info">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-            <span id="chat-info-label">This chatbot needs to be trained, so its answers may not be correct</span>
-        </div>
         <div id="chat-content" style="overflow-y: auto; height: 50vh;">
           <div class="chat chat-start"></div>
           <div class="chat chat-start" id="g">
@@ -62,7 +58,6 @@ utils.onLoad(() => {
 })
 
 function addMessage(sender, content, loading, messageId) {
-    utils.hide('chat-info')
     console.log(messageId == undefined)
     if (messageId == undefined) {
             let customId = utils.mathRandomInt(1, 8759423);
